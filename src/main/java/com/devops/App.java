b5590b0ff614801e987b8f09be94cfa94548c7ad
subbumadu <subbumadu8@gmail.com>
2022-12-11 15:10:20 +0000

    okk

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! iam editing content from CLI to check changes in maven project" );
+        System.out.println( "Hello World! iam editing content from CLI to check changes in maven project rathode wins" );
     }
 }
